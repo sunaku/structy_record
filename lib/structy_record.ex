75567bdb2d6a8b5336107dcff019695afb9c16e1
Suraj N. Kurapati <sunaku@riseup.net>
2019-10-19 16:45:30 -0700

    StructyRecord.defmodule() with zero record fields

--- lib/structy_record.ex
+++ lib/structy_record.ex
@@ -3,16 +3,27 @@ defmodule StructyRecord do
   Documentation for StructyRecord.
   """
 
-  @doc """
-  Hello world.
+  defmacro defmodule(alias = {:__aliases__, _line, [name]}, fields, do_block) do
+    target_module = Module.concat([__CALLER__.module, name])
+    record_module = Module.concat([target_module, :StructyRecord])
 
-  ## Examples
+    quote do
+      defmodule unquote(record_module) do
+        require Record
+        Record.defrecord(:record, unquote(alias), unquote(fields))
+      end
 
-      iex> StructyRecord.hello()
-      :world
+      defmodule unquote(alias) do
+        require unquote(record_module)
 
-  """
-  def hello do
-    :world
+        # name/0 to create a new record with default values for all fields
+        defmacro record() do
+          unquote(record_module).record()
+          |> Macro.escape()
+        end
+
+        unquote(do_block)
+      end
+    end
   end
 end
--- test/structy_record_test.exs
+++ test/structy_record_test.exs
@@ -2,7 +2,32 @@ defmodule StructyRecordTest do
   use ExUnit.Case
   doctest StructyRecord
 
-  test "greets the world" do
-    assert StructyRecord.hello() == :world
+  defmodule Describe__defmodule_with_zero_record_fields do
+    defmodule Setup do
+      require StructyRecord
+
+      StructyRecord.defmodule Record, [] do
+        def my_function, do: :ok
+      end
+    end
+
+    defmodule Test do
+      use ExUnit.Case, async: true
+
+      describe "defmodule()" do
+        test "injects do..end block into module definition" do
+          assert Setup.Record.my_function() == :ok
+        end
+      end
+
+      require Setup.Record
+      require Setup.Record.StructyRecord
+
+      describe "record/0" do
+        test "to create a new record with default values for all fields" do
+          assert Setup.Record.record() == Setup.Record.StructyRecord.record()
+        end
+      end
+    end
   end
 end
