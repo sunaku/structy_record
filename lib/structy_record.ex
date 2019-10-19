7ffbca6cb6d87845fc5a6b4c49d78796259e30af
Suraj N. Kurapati <sunaku@riseup.net>
2019-10-19 10:30:39 -0700

    mix new structy_record

---
+++ lib/structy_record.ex
@@ -0,0 +1,18 @@
+defmodule StructyRecord do
+  @moduledoc """
+  Documentation for StructyRecord.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> StructyRecord.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/structy_record_test.exs
@@ -0,0 +1,8 @@
+defmodule StructyRecordTest do
+  use ExUnit.Case
+  doctest StructyRecord
+
+  test "greets the world" do
+    assert StructyRecord.hello() == :world
+  end
+end
