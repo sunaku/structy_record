afc1f99570fe586cf3457a51f23aa6a14e0e2ecc
Suraj N. Kurapati <sunaku@riseup.net>
2019-10-20 14:52:17 -0700

    factor out StructyRecord.record() macro delegation

--- lib/structy_record.ex
+++ lib/structy_record.ex
@@ -7,17 +7,9 @@ defmodule StructyRecord do
     target_module = Module.concat([__CALLER__.module, name])
     record_module = Module.concat([target_module, :StructyRecord])
 
-    record_module_macro_call =
-      {
-        _call = :.,
-        _meta = [],
-        _args = [record_module, :record]
-      }
-      |> Macro.escape()
-
-    record_macros = macros(record_module, record_module_macro_call)
-    field_getters = fields |> Enum.map(&getter(&1, record_module_macro_call))
-    field_setters = fields |> Enum.map(&setter(&1, record_module_macro_call))
+    record_macros = macros(record_module)
+    field_getters = fields |> Enum.map(&getter(&1, record_module))
+    field_setters = fields |> Enum.map(&setter(&1, record_module))
 
     quote do
       defmodule unquote(record_module) do
@@ -34,7 +26,7 @@ defmodule StructyRecord do
     end
   end
 
-  defp macros(record_module, record_module_macro_call) do
+  defp macros(record_module) do
     quote do
       require unquote(record_module)
 
@@ -42,54 +34,62 @@ defmodule StructyRecord do
         quote do
           call(unquote(args))
         end
-        |> case do
-          {_call, meta, args} ->
-            call = unquote(record_module_macro_call)
-            {call, meta, args}
-        end
+        |> unquote(delegation(record_module))
       end
 
       defmacro record(record, args) do
         quote do
           call(unquote(record), unquote(args))
         end
-        |> case do
-          {_call, meta, args} ->
-            call = unquote(record_module_macro_call)
-            {call, meta, args}
-        end
+        |> unquote(delegation(record_module))
       end
     end
   end
 
-  defp getter(field, record_module_macro_call) do
+  defp getter(field, record_module) do
     quote do
       defmacro unquote(field)(record) do
         quote do
           call(unquote(record), :field)
         end
         |> case do
-          {_call, meta, _args = [record, :field]} ->
-            call = unquote(record_module_macro_call)
+          {call, meta, _args = [record, :field]} ->
             args = [record, unquote(field)]
             {call, meta, args}
         end
+        |> unquote(delegation(record_module))
       end
     end
   end
 
-  defp setter(field, record_module_macro_call) do
+  defp setter(field, record_module) do
     quote do
       defmacro unquote(field)(record, value) do
         quote do
           call(unquote(record), unquote(value))
         end
         |> case do
-          {_call, meta, _args = [record, value]} ->
-            call = unquote(record_module_macro_call)
+          {call, meta, _args = [record, value]} ->
             args = [record, [{unquote(field), value}]]
             {call, meta, args}
         end
+        |> unquote(delegation(record_module))
+      end
+    end
+  end
+
+  defp delegation(record_module) do
+    delegated_call =
+      {
+        _call = :.,
+        _meta = [],
+        _args = [record_module, :record]
+      }
+      |> Macro.escape()
+
+    quote do
+      case do
+        {_call, meta, args} -> {unquote(delegated_call), meta, args}
       end
     end
   end
