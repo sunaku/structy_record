fec6161abad41fe1cde79f90aafc3ee6153e62f9
Suraj N. Kurapati <sunaku@riseup.net>
2019-10-25 23:50:01 -0700

    Version 0.1.0

--- mix.exs
+++ mix.exs
@@ -1,29 +1,47 @@
 defmodule StructyRecord.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @github "https://github.com/sunaku/structy_record"
+
   def project do
     [
       app: :structy_record,
-      version: "0.1.0",
-      elixir: "~> 1.9",
+      version: @version,
+      elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+      package: package(),
+      name: "StructyRecord",
+      description: "Provides a Struct-like interface for your Records."
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:ex_doc, "~> 0.21", only: :dev, runtime: false}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp docs do
+    [
+      source_ref: "v#{@version}",
+      source_url: @github,
+      extras: ["README.md"],
+      main: "readme"
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["ISC"],
+      files: ["LICENSE", "README.md", "lib", "mix.exs"],
+      links: %{"GitHub" => @github}
     ]
   end
 end
